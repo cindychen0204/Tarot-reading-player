e214056411d1e2ff6bd48f8b2d305900f483bf1d
Cindy Chen <cindychen0204@hololab.co.jp>
2020-01-10 23:55:00 +0900

    fix ios build problem

--- tarot-reading-player/Assets/App/Scripts/Finders/IFindCardInTarotDatabase.cs
+++ tarot-reading-player/Assets/App/Scripts/Finders/IFindCardInTarotDatabase.cs
@@ -5,8 +5,6 @@ namespace TarotReadingPlayer.Information.Reader
 {
     public interface IFindCardInformation
     {
-        void ReadFromDatabase();
-
         TarotCard FindCardAllInformation(string cardName);
 
         TarotCard FindTarotCardByNameAndDirection(string cardName, CardDirection direction, Vector3 position);
--- tarot-reading-player/Assets/App/Scripts/Finders/TarotCardFinder.cs
+++ tarot-reading-player/Assets/App/Scripts/Finders/TarotCardFinder.cs
@@ -1,4 +1,5 @@
-﻿using TarotReadingPlayer.Information.Editor;
+﻿using System;
+using TarotReadingPlayer.Information.Editor;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,65 +11,65 @@ namespace TarotReadingPlayer.Information.Reader
 
         private static string DATABASE_PATH = @"Assets/Database/TarotDB.asset";
 
-        public void ReadFromDatabase()
-        {
-            TarotDatabaseObject =
-                (TarotCardDatabaseObject) AssetDatabase.LoadAssetAtPath(DATABASE_PATH, typeof(TarotCardDatabaseObject));
-        }
-
         public TarotCard FindCardAllInformation(string cardName)
         {
-            if (TarotDatabaseObject == null)
+            try
             {
-                ReadFromDatabase();
-            }
-
-            var dummyTarot = new TarotCard();
-            foreach (var card in TarotDatabaseObject.database)
-            {
-                if (TryGetCardInformationInCardDataBase(card, cardName))
+                var dummyTarot = new TarotCard();
+                foreach (var card in TarotDatabaseObject.database)
                 {
-                    dummyTarot = new TarotCard(card.cardName, card.cardEngName, card.number, card.keyword,
-                        card.curSituation_up, card.curSituation_re, card.feelings_up, card.feelings_re, card.cause_up,
-                        card.cause_re, card.future_up, card.future_re,
-                        card.advice_up, card.advice_re, card.love_up, card.love_re, card.work_up, card.work_re,
-                        card.interpersonal_up, card.interpersonal_re, card.other_up, card.other_re);
-                    break;
+                    if (TryGetCardInformationInCardDataBase(card, cardName))
+                    {
+                        dummyTarot = new TarotCard(card.cardName, card.cardEngName, card.number, card.keyword,
+                            card.curSituation_up, card.curSituation_re, card.feelings_up, card.feelings_re, card.cause_up,
+                            card.cause_re, card.future_up, card.future_re,
+                            card.advice_up, card.advice_re, card.love_up, card.love_re, card.work_up, card.work_re,
+                            card.interpersonal_up, card.interpersonal_re, card.other_up, card.other_re);
+                        break;
+                    }
                 }
+                return dummyTarot;
             }
-            return dummyTarot;
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         public TarotCard FindTarotCardByNameAndDirection(string cardName, CardDirection direction, Vector3 position)
         {
-            if (TarotDatabaseObject == null)
+            try
             {
-                ReadFromDatabase();
-            }
-
-            var dummyTarot = new TarotCard();
-            foreach (var card in TarotDatabaseObject.database)
-            {
-                if (TryGetCardInformationInCardDataBase(card, cardName))
+                var dummyTarot = new TarotCard();
+                foreach (var card in TarotDatabaseObject.database)
                 {
-                    switch (direction)
+                    if (TryGetCardInformationInCardDataBase(card, cardName))
                     {
-                        case CardDirection.Default:
-                            break;
-                        case CardDirection.Upright:
-                            dummyTarot = new TarotCard(card.cardName, card.cardEngName, card.keyword, direction, position,
-                                card.curSituation_up, card.feelings_up, card.cause_up, card.future_up,
-                                card.advice_up, card.love_up, card.work_up, card.interpersonal_up, card.other_up);
-                            break;
-                        case CardDirection.Reversed:
-                            dummyTarot = new TarotCard(card.cardName, card.cardEngName, card.keyword, direction, position,
-                                card.curSituation_re, card.feelings_re, card.cause_re, card.future_re,
-                                card.advice_re, card.love_re, card.work_re, card.interpersonal_re, card.other_re);
-                            break;
+                        switch (direction)
+                        {
+                            case CardDirection.Default:
+                                break;
+                            case CardDirection.Upright:
+                                dummyTarot = new TarotCard(card.cardName, card.cardEngName, card.keyword, direction, position,
+                                    card.curSituation_up, card.feelings_up, card.cause_up, card.future_up,
+                                    card.advice_up, card.love_up, card.work_up, card.interpersonal_up, card.other_up);
+                                break;
+                            case CardDirection.Reversed:
+                                dummyTarot = new TarotCard(card.cardName, card.cardEngName, card.keyword, direction, position,
+                                    card.curSituation_re, card.feelings_re, card.cause_re, card.future_re,
+                                    card.advice_re, card.love_re, card.work_re, card.interpersonal_re, card.other_re);
+                                break;
+                        }
                     }
                 }
+                return dummyTarot;
             }
-            return dummyTarot;
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         private bool TryGetCardInformationInCardDataBase(TarotCardInformation card, string cardName)
